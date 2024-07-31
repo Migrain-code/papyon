daced0f431fcb25c20cb4a4988947394b018f67c
migrain.code <migrain.code@gmail.com>
2024-07-31 15:41:57 +0300

    -- canlıdaki hatalar düzeltildi

--- app/Http/Controllers/PlaceMenuController.php
+++ app/Http/Controllers/PlaceMenuController.php
@@ -3,25 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Place;
+use App\Models\PlaceVisitor;
 use App\Models\Table;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class PlaceMenuController extends Controller
 {
-    public function index($slug)
+    public function index($slug, Request $request)
     {
         $place = Place::whereSlug($slug)->first();
+        $placeVisitor = new PlaceVisitor();
+        $placeVisitor->place_id = $place->id;
+        $placeVisitor->ip_address = $request->ip();
+        $placeVisitor->save();
         Session::put('place', $place);
         return to_route('menu.index', $place->slug);
     }
 
-    public function table($code)
+    public function table($code, Request $request)
     {
         $table = Table::where('qr_name', $code)->first();
+        $place = $table->place;
+        $placeVisitor = new PlaceVisitor();
+        $placeVisitor->place_id = $place->id;
+        $placeVisitor->ip_address = $request->ip();
+        $placeVisitor->save();
         Session::put('table', $table);
-        Session::put('place', $table->place);
-        return to_route('menu.index', $table->place->slug);
+        Session::put('place',  $place);
+        return to_route('menu.index', $place->slug);
     }
 
     public function notify()
