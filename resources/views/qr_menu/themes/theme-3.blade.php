dff2004644030b703d3bc1fa653f80637a988189
migrain.code <migrain.code@gmail.com>
2024-07-31 23:58:39 +0300

    -- canlıdaki hatalar düzeltildi

--- resources/views/qr_menu/themes/theme-3.blade.php
+++ resources/views/qr_menu/themes/theme-3.blade.php
@@ -130,7 +130,32 @@
             @include('qr_menu.themes.parts.swiper')
         @endif
     </div>
+    @if(isset($menu->banner))
+        <div class="dialogModal">
+            <div class="modal fade" id="popupBannerModal" data-bs-backdrop="static" data-bs-keyboard="false"
+                 tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content"  style="border: none">
 
+                        <div class="modal-body " @if($menu->banner->banner_type == 1 || $menu->banner->banner_type == 2) style="border-radius: 7px;min-height: 250px;background-image: url('{{storage($menu->banner->image)}}') !important;background-size: cover;" @endif >
+                            <button type="button" class="btn-close closeButton" style="position: absolute;
+                                    right: 10px;
+                                    top: 10px;
+                                    padding: 5px;" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                        </div>
+                        @if($menu->banner->banner_type == 1 || $menu->banner->banner_type == 3)
+                            <div class="modal-footer" style="justify-content: start;padding: 20px;">
+                                <div class="text-start" style="overflow-wrap: anywhere;"> {{ $menu->banner->description }}</div>
+                            </div>
+                        @endif
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
 
 @section('scripts')
@@ -163,4 +188,14 @@
         });
 
     </script>
+
+    @if(isset($menu->banner))
+        <script>
+            if (!localStorage.getItem('modalShown')) {
+                var productModal = new bootstrap.Modal(document.querySelector('#popupBannerModal'));
+                productModal.show();
+                localStorage.setItem('modalShown', 'true');
+            }
+        </script>
+    @endif
 @endsection
