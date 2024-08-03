24e6f2c88c48976ee00ffb80f150bf4e754b1256
migrain.code <migrain.code@gmail.com>
2024-08-03 19:30:51 +0300

    -- hatalar fixlendi

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,61 +1,41 @@
-@extends('qr_menu.layouts.master')
-@section('title', 'Uyarı')
-@section('styles')
-    <style>
-        .bg-label-warning {
-            background-color: #fff1e3 !important;
-            color: #ff9f43 !important;
-        }
-        .bg-label-success {
-            background-color: #dff7e9 !important;
-            color: #28c76f !important;
-        }
-        .bg-label-danger {
-            background-color: #fce5e6 !important;
-            color: #ea5455 !important;
-        }
-        .bg-label-secondary {
-            background-color: #f2f2f3 !important;
-            color: #a8aaae !important;
-        }
-        .bg-label-info {
-            background-color: #d9f8fc !important;
-            color: #00cfe8 !important;
-        }
-        .bg-label-success {
-            background-color: #dff7e9 !important;
-            color: #28c76f !important;
-        }
-    </style>
-@endsection
-@section('content')
-    <section class="waiter">
-        <div class="content">
-            <svg xmlns="http://www.w3.org/2000/svg" width="149.567" height="149.581" viewBox="0 0 149.567 149.581">
-                <g id="IVGST5.tif" transform="translate(-12482.013 -8218.461)">
-                    <g id="Group_2071" data-name="Group 2071">
-                        <g id="Group_2069" data-name="Group 2069">
-                            <path id="Path_2227" data-name="Path 2227"
-                                  d="M12631.58,8293.183a74.784,74.784,0,1,1-74.806-74.722A74.875,74.875,0,0,1,12631.58,8293.183Zm-10.28.03a64.5,64.5,0,1,0-64.513,64.548A64.67,64.67,0,0,0,12621.3,8293.213Z"
-                                  fill="#f3f3f1" />
-                        </g>
-                        <g id="Group_2070" data-name="Group 2070">
-                            <path id="Path_2228" data-name="Path 2228"
-                                  d="M12549.655,8320.689l-24.44-20.789,6.485-7.819,17.459,14.615c13.484-13.65,26.9-27.233,40.411-40.9,2.377,2.509,4.658,4.918,7.045,7.436Z"
-                                  fill="#f3f3f1" />
-                        </g>
-                    </g>
-                </g>
-            </svg>
+<html>
+    <head>
+        <style>
+            body{
+                padding: 5px;
 
-                <h2 style="text-align: center;">
-                    İşletmeye Erişemezsiniz
-                </h2>
-                <h2>İşletme Henüz Kurulumunu Tamamlamamış veya menü eklememiş olabilir.</h2>
+            }
+            .container{
+                width: 100%;
+                height: 100%;
+                background: #e0483d;
+                color: white;
+                border-radius: 15px;
+                padding: 5px;
+                text-align: center;
+                position: relative;
+                font-family: "Roboto", sans-serif;
+            }
+            .centerBox{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                min-width: 280px;
+            }
+
+        </style>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
+
+    </head>
+<body>
+    <div class="container">
+        <div class="centerBox">
+            <i class="ti ti-hand-off" style="font-size: 5rem"></i>
+            <h2 class="title">İşletmeye Erişemezsiniz</h2>
+            <b>Lütfen menüyü görebilmek için menüye kategori ve ürün ekleyin.</b>
         </div>
-    </section>
-@endsection
-
-@section('scripts')
-
-@endsection
+    </div>
+</body>
+</html>
