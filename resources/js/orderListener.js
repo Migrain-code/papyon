679f925242fb8737fadd38171001220a55a58ecb
migrain.code <migrain.code@gmail.com>
2024-10-30 01:05:23 +0300

    -- sokeet bağlantı testi 6

--- resources/js/orderListener.js
+++ resources/js/orderListener.js
@@ -11,8 +11,27 @@ Echo.channel('order-channel')
         });
 
         // Gelen veriyi kontrol et
-        console.log('gelenveri', e.ordersCount);
+        const orderData = e;
 
+        const ordersCount = orderData.ordersCount;
+        const packetCount = orderData.packetCount;
+        const taxiCount = orderData.taxiCount;
+        const valeCount = orderData.valeCount;
+        const waiterCount = orderData.waiterCount;
+        const totalClaims = orderData.totalClaims;
+        // Değerleri uygun div'lere yazdır
+        $('#newClaimCount').text(ordersCount);
+        $('#orderCount').text(ordersCount);
+        $('#packetCount').text(packetCount);
+        $('#taxiCount').text(taxiCount);
+        $('#valeCount').text(valeCount);
+        $('#waiterCount').text(waiterCount);
+        $('#totalClaims').text(totalClaims);
+
+        // Ses çal
+        audio.play().catch((error) => {
+            console.error("Audio playback failed:", error);
+        });
     });
 
 
