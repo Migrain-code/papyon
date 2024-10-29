26467b775f60fb888c29039e2b195faf94fc107f
migrain.code <migrain.code@gmail.com>
2024-10-30 00:33:36 +0300

    -- sokeet bağlantı testi 2

--- resources/js/orderListener.js
+++ resources/js/orderListener.js
@@ -1,38 +1,19 @@
 // Import the Echo instance
 import Echo from './echo';
 
+const audio = document.getElementById('success-sound');
+
 // Listen for the 'order.created' event
 Echo.channel('order-channel')
     .listen('.order.created', (e) => {
-        console.log("Order created event received", e); // Debug log
+        console.log("Order created event received", e); // Burada veriyi görebilirsiniz
         Toast.fire({
                  icon: 'info',
                  title: 'Yeni siparişiniz var',
              });
-        // Gelen verileri JSON formatında parse et
-        const data = JSON.parse(e.data);
-
-        // Değerleri al
-        const ordersCount = data.order.ordersCount;
-        const packetCount = data.order.packetCount;
-        const taxiCount = data.order.taxiCount;
-        const valeCount = data.order.valeCount;
-        const waiterCount = data.order.waiterCount;
-        const totalClaims = data.order.totalClaims;
-
-        // Değerleri uygun div'lere yazdır
-        $('#newClaimCount').text(ordersCount);
-        $('#orderCount').text(ordersCount);
-        $('#packetCount').text(packetCount);
-        $('#taxiCount').text(taxiCount);
-        $('#valeCount').text(valeCount);
-        $('#waiterCount').text(waiterCount);
-        $('#totalClaims').text(totalClaims);
-
-        // Ses çal
-        audio.play().catch((error) => {
-            console.error("Audio playback failed:", error);
-        });
+        if (e.data) {
+            console.log("Received data:", e.data); // Veriyi kontrol et
+        }
+        audio.play().catch(error => console.log("Playback error:", error));
     });
-
 
