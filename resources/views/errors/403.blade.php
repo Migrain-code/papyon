73623b4b30b169c688fd474c6075f07cda3517f8
migrain.code <migrain.code@gmail.com>
2024-10-30 01:48:25 +0300

    -- sokeet bağlantı testi tamamlandı

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -11,10 +11,9 @@
                 <div class="w-500px text-center border-1">
                     <img src="/business/assets/media/logos/vector.jpg" style="width: 100%">
                     <h1 class="fs-2hx">Erişim Reddedildi</h1>
-                    <span class="fs-2">Bu özelliğe erişmek istiyorsanız <a href="{{route('business.subscription.index')}}">abonelik</a> sayfasından paket yükseltme yapabilirsiniz</span>
+                    <span class="fs-2">Bu özelliğe erişemezsiniz</span>
                     <div class="d-flex justify-content-center gap-3 my-6">
                         <a href="{{url()->previous()}}" class="btn btn-light-info fs-3"> Geri Dön </a>
-                        <a href="{{route('business.subscription.index')}}" class="btn btn-light-success fs-3">Paket Yükselt <i class="fa fa-fire"></i></a>
                     </div>
                 </div>
             </div>
