63d0065a5307ac28ffcd8ee58c95782d2a07227e
migrain.code <migrain.code@gmail.com>
2024-10-30 01:02:53 +0300

    -- sokeet bağlantı testi 6

--- resources/js/orderListener.js
+++ resources/js/orderListener.js
@@ -11,42 +11,8 @@ Echo.channel('order-channel')
         });
 
         // Gelen veriyi kontrol et
-        if (e) {
-            try {
-                // Order verisini kontrol et
-                if (e) {
-                    const orderData = JSON.parse(e);
+        console.log('gelenveri', e.ordersCount);
 
-                    // Değerleri al
-                    const ordersCount = orderData.ordersCount;
-                    const packetCount = orderData.packetCount;
-                    const taxiCount = orderData.taxiCount;
-                    const valeCount = orderData.valeCount;
-                    const waiterCount = orderData.waiterCount;
-                    const totalClaims = orderData.totalClaims;
-
-                    // Değerleri uygun div'lere yazdır
-                    $('#newClaimCount').text(ordersCount);
-                    $('#orderCount').text(ordersCount);
-                    $('#packetCount').text(packetCount);
-                    $('#taxiCount').text(taxiCount);
-                    $('#valeCount').text(valeCount);
-                    $('#waiterCount').text(waiterCount);
-                    $('#totalClaims').text(totalClaims);
-
-                    // Ses çal
-                    audio.play().catch((error) => {
-                        console.error("Audio playback failed:", error);
-                    });
-                } else {
-                    console.error("Order verisi tanımsız:", data);
-                }
-            } catch (error) {
-                console.error("JSON ayrıştırma hatası:", error);
-            }
-        } else {
-            console.error("Gelen data tanımsız veya boş:", e);
-        }
     });
 
 
